d6c3e39def28fa1df34e066cce05a9f609501df9
Ryosuke Ito <40818197+ryosuke110@users.noreply.github.com>
2026-05-12 11:36:20 +0900

    Update Genomic_cline_pymc.py

--- 02_population_structure_hybridisation/Genomic_cline_pymc.py
+++ 02_population_structure_hybridisation/Genomic_cline_pymc.py
@@ -28,14 +28,12 @@ y = np.clip(hi_raw, eps, 1 - eps)
 with pm.Model() as hi_null_model:
     mu0 = pm.Beta("mu0", alpha = 2, beta = 2)
     kappa0 = pm.Gamma("kappa0", alpha = 2.0, beta = 0.5)
-
     pm.Beta(
         "HI",
         alpha = mu0 * kappa0,
         beta = (1 - mu0) * kappa0,
         observed = y
     )
-
     idata_null = pm.sample(
         draws = 20000,
         tune = 20000,
@@ -50,18 +48,15 @@ with pm.Model() as hi_dist_model:
     m_d = pm.Normal("center_dist", mu = 1560, sigma = 250)
     w_d = pm.HalfNormal("width_dist", sigma = 5.0)
     s = pm.Normal("slope_sign", 0.0, 1.0)
-
     eta = s * (dist - m_d) / w_d
     mu = pm.Deterministic("mu", pm.math.sigmoid(eta))
     kappa = pm.Gamma("kappa", alpha = 2.0, beta = 0.5)
-
     pm.Beta(
         "HI",
         alpha = mu * kappa,
         beta = (1 - mu) * kappa,
         observed = y
     )
-
     idata_dist = pm.sample(
         draws = 20000,
         tune = 20000,
@@ -75,18 +70,15 @@ with pm.Model() as hi_dist_model:
 with pm.Model() as hi_elev_model:
     m_e = pm.Normal("center_elev", mu = 378.0, sigma = 50.0)
     w_e = pm.HalfNormal("width_elev", sigma = 200.0)
-
     eta = (elev - m_e) / w_e
     mu = pm.Deterministic("mu", pm.math.sigmoid(eta))
     kappa = pm.Gamma("kappa", alpha = 2.0, beta = 0.5)
-
     pm.Beta(
         "HI",
         alpha = mu * kappa,
         beta = (1 - mu) * kappa,
         observed = y
     )
-
     idata_elev = pm.sample(
         draws = 20000,
         tune = 20000,
