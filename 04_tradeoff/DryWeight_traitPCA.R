900c3b20109c0d4667eb69564ee2f69fdcd15fe8
Ryosuke Ito <40818197+ryosuke110@users.noreply.github.com>
2026-05-12 18:21:14 +0900

    Update DryWeight_traitPCA.R

--- 04_tradeoff/DryWeight_traitPCA.R
+++ 04_tradeoff/DryWeight_traitPCA.R
@@ -2,6 +2,7 @@
 # Fit a GAM for total dry weight using PCA traits
 # Author: Ryosuke Ito
 
+library(data.table)
 library(mgcv)
 
 ### Input ###
@@ -14,10 +15,10 @@ df <- fread(infile)
 
 ### Prepare data ###
 pc_cols <- paste0("PC", 1:10)
-need_cols <- c(pc_cols, "HeatShock", "Density", "Dwtot")
+need_cols <- c(pc_cols, "Treatment", "Density", "Dwtot")
 
-df_model <- df[, need_cols]
-df_model$HeatShock <- factor(as.integer(df_model$HeatShock))
+df_model <- df[, ..need_cols]
+df_model$Treatment <- factor(as.integer(df_model$Treatment))
 df_model$Density <- factor(as.integer(df_model$Density))
 df_model$Dwtot <- suppressWarnings(as.numeric(df_model$Dwtot))
 
@@ -28,13 +29,13 @@ fit <- gam(
   Dwtot ~
     s(PC1) + s(PC2) + s(PC3) + s(PC4) + s(PC5) +
     s(PC6) + s(PC7) + s(PC8) + s(PC9) + s(PC10) +
-    HeatShock + Density,
+    Treatment + Density,
   data = df_model,
   method = "REML"
 )
 
 fit_sum <- summary(fit)
-gam.check(fit)
+AIC(fit)
 
 ### Extract parametric terms ###
 parametric_df <- data.frame(
